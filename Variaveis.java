a790ae561584f5c04131342f8658f7fbf1a5b80c
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-21 20:40:04 -0300

    modelo de uso de variaveis em java

--- Variaveis.java
+++ Variaveis.java
@@ -1,7 +1,11 @@
 public class Variaveis {
     public static void main(String[] args) {
-        String name = "juan";
-        System.out.println(name);
+        String name = "juan ";
+        int num = 8;
+        int x = 5;
+        int y = 19;
+        int soma = num + x + y ;
+        System.out.println("nome do individuo " + name + soma );
     }
     
 }
