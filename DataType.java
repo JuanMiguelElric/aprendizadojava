2b30565d658aece963e185ba3e3763023898f827
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-22 00:37:39 -0300

    datatype

---
+++ DataType.java
@@ -0,0 +1,10 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        short myNum = 500;
+        double  dec = 19.49d;
+
+        System.out.println("Isso é inteiro" + myNum + " isso é decimal" + dec);
+    }
+    
+}
