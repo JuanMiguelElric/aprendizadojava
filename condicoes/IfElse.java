4721402808a1958312267b53db9534fb2a2bdbe2
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-28 17:06:02 -0300

    condições em java

---
+++ condicoes/IfElse.java
@@ -0,0 +1,17 @@
+package condicoes;
+
+public class IfElse {
+    public static void main(String[] args) {
+        String nome = "juan";
+        int idade = 21;
+
+        if (nome == "juan" && idade >= 23){
+
+            System.out.printf("acertou no nome");
+
+        }else{
+            System.out.printf("os dados não batem");
+        }
+
+    }
+}
